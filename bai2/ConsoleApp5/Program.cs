2bb3ad60bffb38d4d7a16eff856fc527512ba027
daonhat198 <161843260+daonhat198@users.noreply.github.com>
2024-09-05 18:16:56 +0700

    Add files via upload

---
+++ bai2/ConsoleApp5/Program.cs
@@ -0,0 +1,17 @@
+﻿internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Nhap mot chuoi :");
+        string input = Console.ReadLine();
+        int dem = 0;
+        foreach(char c in input)
+        {
+            if (char.IsLetterOrDigit(c))
+            {
+                dem++;
+            }
+        }
+        Console.WriteLine("So luong ki tu trong chuoi la :" +dem);
+    }
+}
