75b8e850c1f7f826e766a4b188fecf712d8edbed
daonhat198 <161843260+daonhat198@users.noreply.github.com>
2024-09-05 18:09:53 +0700

    Add files via upload

---
+++ bai1/ConsoleApp4/Program.cs
@@ -0,0 +1,20 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Nhap so luong phan tu cua mang :");
+        int n = int.Parse(Console.ReadLine());
+        int[] A = new int[n];
+        for (int i = 0; i < n; i++)
+        {
+            Console.Write("Nhap cac phan tu mang :");
+            A[i] = int.Parse(Console.ReadLine());
+        }
+        int tong = 0;
+        for (int i = 0; i < n; i++)
+        {
+            tong += A[i];
+        }
+        Console.WriteLine("Tong cac phan tu cua mang la:" + tong);
+    }
+}
