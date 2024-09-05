b3b2bc805d0b2a1833dec31e2f7413ccc4deba57
daonhat198 <161843260+daonhat198@users.noreply.github.com>
2024-09-05 18:22:26 +0700

    Add files via upload

---
+++ bai6/ConsoleApp9/Program.cs
@@ -0,0 +1,21 @@
+﻿internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Nhap mot chuoi :");
+        string input = Console.ReadLine();
+
+        Console.WriteLine("Nhap ky tu can dem :");
+        char kt = char.Parse(Console.ReadLine());
+
+        int dem = 0;
+        foreach(char c in input)
+        {
+            if(c == kt)
+            {
+                dem++;
+            }
+        }
+        Console.WriteLine("So lan ky tu xuat hien la :" +dem);  
+    }
+}
