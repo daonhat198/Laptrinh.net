ec4d5b7a7cdcaf7fc3e2e772f52259c3320c0a94
daonhat198 <161843260+daonhat198@users.noreply.github.com>
2024-09-05 18:21:20 +0700

    Add files via upload

---
+++ bai5/ConsoleApp8/Program.cs
@@ -0,0 +1,32 @@
+﻿internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Nhap so pha tu mang :");
+        int n = int.Parse(Console.ReadLine());
+        int[] A = new int[n];
+        for (int i = 0; i < n; i++)
+        {
+            Console.WriteLine("Nhap cac phan tu mang :");
+            A[i] = int.Parse(Console.ReadLine());
+        }
+        bool kiemtra = true;
+        for (int i = 0; i < n; i++)
+        {
+            if (A[i] != A[n - i - 1])
+            {
+                kiemtra = false;
+                break;
+            }
+        }
+        if (kiemtra)
+        {
+            Console.WriteLine("Mang  doi xung ");
+        }
+        else
+        {
+            Console.WriteLine("Mang khong do xung ");
+        }
+
+    }
+}
