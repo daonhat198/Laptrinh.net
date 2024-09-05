82caf664f636fe09137e48401a3e130958db371b
daonhat198 <161843260+daonhat198@users.noreply.github.com>
2024-09-05 18:20:21 +0700

    Add files via upload

---
+++ bai4/ConsoleApp7/Program.cs
@@ -0,0 +1,13 @@
+﻿internal class Program
+{
+   static void Main(string[] args)
+    {
+        Console.WriteLine("Nhap mot chuoi :");
+        string input = Console.ReadLine();
+        char[] A = input.ToCharArray();
+        Array.Reverse(A);
+       
+        string output = new string(A);
+        Console.WriteLine("Chuoi sau khi dao nguoc :" +output);
+    }
+}
